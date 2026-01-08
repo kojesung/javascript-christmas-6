7ffda8e17d6772ed702dcce7007602d7b7f66f61
kojesung <js95112345@gmail.com>
2026-01-08 15:19:15 +0900

    feat(constants): 도메인에서 사용하는 매직스트링 상수화

---
+++ src/constants.js
@@ -0,0 +1,59 @@
+export const BADGE = { STAR: '별', TREE: '트리', SANTA: '산타', NONE: '없음' };
+export const BADGE_CUT_LINE = { STAR: 5000, TREE: 10000, SANTA: 20000 };
+
+export const STAR_DAY = [3, 10, 17, 24, 25, 31];
+
+export const DEFAULT_DC = 1000;
+export const SURCHARGE_DC = 100;
+
+export const MENU_LIST = [
+    '양송이수프',
+    '타파스',
+    '시저샐러드',
+    '티본스테이크',
+    '바비큐립',
+    '해산물파스타',
+    '크리스마스파스타',
+    '초코케이크',
+    '아이스크림',
+    '제로콜라',
+    '레드와인',
+    '샴페인',
+];
+
+export const DRINKS = ['제로콜라', '레드와인', '샴페인'];
+
+export const MAINS = ['티본스테이크', '바비큐립', '해산물파스타', '크리스마스파스타'];
+
+export const DESERTS = ['초코케이크', '아이스크림'];
+
+export const MENU_PRICE = {
+    양송이수프: 6000,
+    타파스: 5500,
+    시저샐러드: 8000,
+    티본스테이크: 55000,
+    바비큐립: 54000,
+    해산물파스타: 35000,
+    크리스마스파스타: 25000,
+    초코케이크: 15000,
+    아이스크림: 5000,
+    제로콜라: 3000,
+    레드와인: 60000,
+    샴페인: 25000,
+};
+
+export const EVENT_CUT_LINE = 10000;
+export const MAX_MENU_COUNT = 20;
+
+export const CHRISTMAS_EVENT = {
+    START: 1,
+    END: 25,
+};
+export const DECEMBER_EVENT = {
+    START: 1,
+    END: 31,
+};
+
+export const DECEMBER_EVENT_DC = 2023;
+
+export const CHAMPAGNE_CUT_LINE = 120000;
